6664ccf9bdda219b0a46fcd3261cf99fdae00652
William Henrotin <whe@odoo.com>
2024-05-31 11:44:52 +0200

    new remaps and options

--- .config/nvim/lua/whenrow/remap.lua
+++ .config/nvim/lua/whenrow/remap.lua
@@ -8,16 +8,14 @@ vim.keymap.set("", "<Esc><Esc>", vim.cmd.w)
 -- Quickfix list
 vim.keymap.set("n", "]c", ":cnext<CR>zz", {silent = true})
 vim.keymap.set("n", "[c", ":cprevious<CR>zz", {silent = true})
+-- Tab navigation
+vim.keymap.set("n", "]t", ":tabNext<CR>", {silent = true})
+vim.keymap.set("n", "[t", ":tabprevious<CR>", {silent = true})
 -- split line at cursor
-vim.keymap.set("n", "K", ":i<CR><esc>")
+vim.keymap.set("n", "K", "i<CR><esc>")
 -- Center next highlighted word
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
-vim.keymap.set("n", "-", vim.cmd.Vex, {silent = true})
--- Edit nvim config
-vim.keymap.set("n", "<leader>ce", function() vim.cmd.Vexplore("~/.config/nvim/lua/whenrow") end)
--- Edit lua config
-vim.keymap.set("n", "<leader>cl", function() vim.cmd.edit("~/.config/nvim/lua/whenrow/init.lua") end)
 -- open zshrc
 vim.keymap.set("n", "<leader>cz", function() vim.cmd.edit("~/.zshrc") end)
 -- Python debugging made easy
@@ -26,43 +24,13 @@ vim.cmd.iabbrev("pudb", "import pudb;pudb.set_trace()")
 vim.keymap.set("n", "<leader>sd", function() vim.cmd.cd("%:p:h") end)
 -- Copy current file path to clipboard
 vim.keymap.set("n", "<leader>cp", function() vim.cmd.let("@+ = expand('%:p')") end)
-
-
-
-
-
---""open .config/ folder in FZF
---"nmap <leader>cc :lua require'telescope.builtin'.find_files({search_dirs={'~/.config/'},hidden=true})<cr>
---""
---
---
---
---
--- -- Trailing whitespace at save
--- autocmd BufWritePre * %s/\s\+$//e
-
-
-
-
---""==================
---"" Tree view
---""==================
---"let g:netrw_banner = 0
---"let g:netrw_liststyle = 3
---"let g:netrw_browse_split = 4
---"let g:netrw_altv = 1
---"let g:netrw_winsize = 20
---"let g:netrw_list_hide= '.*\.pyc$,_pycache__'
-
---""==================
---"" Greper
---""===================
---"nnoremap <Leader>/ :Telescope live_grep<CR>
---"nnoremap <Leader>gf :Telescope grep_string<CR>
-
---""==================
---"" Snippets
---""===================
---""" Use <C-l> for trigger snippet expand.
---"" imap <silent> <C-l> <Plug>(coc-snippets-expand)
-
+-- Open stack trace line
+vim.keymap.set("n", "<leader>s", function ()
+    local stack_line = vim.api.nvim_get_current_line()
+    local file, line = string.match(stack_line, 'File "([%w./-_]+)", line (%d+), in')
+    if file and line then
+        vim.api.nvim_command("e +" .. line .. " " .. file)
+    end
+end)
+-- source current file
+vim.keymap.set("n", "<F5>", vim.cmd.source)
--- .config/nvim/lua/whenrow/set.lua
+++ .config/nvim/lua/whenrow/set.lua
@@ -1,16 +1,22 @@
+vim.opt.autoread = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.opt.expandtab = true
+vim.opt.hidden = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.autoread = true
-vim.opt.hidden = true
-vim.opt.expandtab = true
+vim.opt.scrolloff = 10
 vim.opt.shiftwidth=4
 vim.opt.softtabstop=4
 vim.opt.spell = true
 vim.opt.swapfile = false
 vim.opt.tags = ".tags"
 vim.opt.termguicolors = true
+vim.opt.textwidth = 100
 vim.cmd([[highlight ColorColumn ctermbg=0 guibg=#313244]])
 vim.opt.cc = "100"
+-- open terminal mode in insert mode
+vim.cmd([[autocmd TermOpen * startinsert]])
 --""==================
 --"" Tree view
 --""==================
@@ -19,3 +25,5 @@ vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 4
 vim.g.netrw_altv = 1
 vim.g.netrw_winsize = 20
+vim.g.unstack_open_tab=0
+vim.g.unstack_populate_quickfix=1
