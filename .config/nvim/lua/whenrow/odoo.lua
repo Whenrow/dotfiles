406b71819079dda73741d8b774c40708b37297b4
William Henrotin <whe@odoo.com>
2024-10-21 14:24:51 +0200

    odools

--- .config/nvim/lua/whenrow/odoo.lua
+++ .config/nvim/lua/whenrow/odoo.lua
@@ -1,8 +1,40 @@
-local openOdoo = function()
-    vim.cmd.tabnew()
-    vim.cmd.terminal("pwd")
-    -- print("coucou odoo")
-end
-vim.api.nvim_create_user_command('Odoo', openOdoo, {})
--- vim.keymap.set("n", "<leader>o", vim.cmd.Odoo)
-
+local lsp_config = require('lspconfig.configs')
+local odooConfig = {
+    id = 1,
+    name = "config 1",
+    addons = {"/home/whe/src/odoo/addons", "/home/whe/src/enterprise"},
+    odooPath = "/home/whe/src/odoo/",
+    pythonPath = "/home/whe/.pyenv/shims/python3",
+}
+lsp_config.odools = {
+    default_config = {
+        name = 'odools',
+        cmd = vim.lsp.rpc.connect('127.0.0.1', 2087),
+        -- cmd = {'/home/whe/src/odools/odoo_ls_server'},
+        root_dir = "/home/whe/src/odoo/",
+        handlers = {
+            ['Odoo/getPythonPath'] = function()
+                return {pythonPath = "/home/whe/.pyenv/shims/python3"}
+            end,
+        },
+        settings = {
+            Odoo = {
+                autoRefresh = true,
+                autoRefreshDelay = nil,
+                diagMissingImportLevel = "none",
+                configurations = { mainConfig = odooConfig },
+                selectedConfiguration = "mainConfig",
+            },
+        },
+        capabilities = {
+            textDocument = {
+                workspace = {
+                    symbol = {
+                        dynamicRegistration = true,
+                    },
+                },
+            },
+        },
+    },
+}
+lsp_config.odools.setup{}
