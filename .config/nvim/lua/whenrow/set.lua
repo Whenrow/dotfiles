cd41aeea2dca4b2546fd1fd823636cf933b4d526
William Henrotin <whe@odoo.com>
2024-10-30 07:27:17 +0100

    no more netrw since oil.nvim

--- .config/nvim/lua/whenrow/set.lua
+++ .config/nvim/lua/whenrow/set.lua
@@ -13,17 +13,8 @@ vim.opt.swapfile = false
 vim.opt.tags = ".tags"
 vim.opt.termguicolors = true
 vim.opt.textwidth = 100
+vim.opt.undofile = true
 vim.cmd([[highlight ColorColumn ctermbg=0 guibg=#313244]])
 vim.opt.cc = "100"
 -- open terminal mode in insert mode
 vim.cmd([[autocmd TermOpen * startinsert]])
---""==================
---"" Tree view
---""==================
-vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_browse_split = 4
-vim.g.netrw_altv = 1
-vim.g.netrw_winsize = 20
-vim.g.unstack_open_tab=0
-vim.g.unstack_populate_quickfix=1
